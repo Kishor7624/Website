5996fc7cf0a3688cecf3d2900a29892345017eec
Kishor <meyyappankishor@gmail.com>
2026-05-15 12:18:50 +0530

    Fetch parts from Supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  process.env.https://ojtgatlqmzdyymvdjtur.supabase.co
-  ,
-  process.env.sb_publishable_2RjSZ6xIZ8BfG4ELTOGnLA_nk4LGxNu
+ process.env.REACT_APP_SUPABASE_URL || 'https://ojtgatlqmzdyymvdjtur.supabase.co',
+ process.env.REACT_APP_SUPABASE_KEY || 'sb_publishable_2RjSZ6xIZ8BfG4ELTOGnLA_nk4LGxNu'
 
 )
